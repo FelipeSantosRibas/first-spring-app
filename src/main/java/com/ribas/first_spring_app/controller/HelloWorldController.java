46810eed5ee4bf636ed3456e786fc446a0ed94bb
FelipeSantosRibas <felipesantosribas2007@gmail.com>
2026-03-04 16:01:04 -0300

    Last commit, Service, Configuration, User and connected it using the Controller

--- src/main/java/com/ribas/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/ribas/first_spring_app/controller/HelloWorldController.java
@@ -1,18 +1,31 @@
 package com.ribas.first_spring_app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ribas.first_spring_app.domain.User;
+import com.ribas.first_spring_app.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController // @Controller + @ResponseBody
 //STATELESS: token a cada solicitação | STATEFULL: login só uma vez
 @RequestMapping("/hello-world") // Escuta todas as requisições que chegarem no endpoint /hello-world
 public class HelloWorldController {
-    // Métodos HTTP: post, get, delete, put, patch, options, head
 
+    @Autowired
+    private HelloWorldService helloWorldService;
+    //public HelloWorldController(HelloWorldService helloWorldService){
+    //    this.helloWorldService = helloWorldService;
+    //}
+
+
+    // Métodos HTTP: post, get, delete, put, patch, options, head
     @GetMapping // Responde as requisições get no endpoint /hello-world
     public String helloWorld(){
-        return "Hello World!";
+        return helloWorldService.helloWorld("Felipe");
+    }
+
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable("id") String id, @RequestParam(value = "filter", defaultValue = "null") String filter, @RequestBody User body){
+        return "Nome: " + body.getName() + "\n Id: " + id + "\n Filter: " + filter;
     }
 }
